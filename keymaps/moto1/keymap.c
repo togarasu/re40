ac95d199bb96ccc3d78c4ed7311d1c4779ea75d1
kogarasu <clha4fn6@gmail.com>
2020-09-20 13:22:44 +0900

    right shift to left alt

--- keymaps/moto1/keymap.c
+++ keymaps/moto1/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |-------+-------+-------+-------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
         TT_CTLR,KC_A   ,KC_O   ,KC_E   ,KC_I   ,KC_U   ,                             KC_G   ,KC_T   ,KC_K   ,KC_S   ,KC_N   ,KC_QUOT,
     // |-------+-------+-------+-------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
-        KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_F   ,                             KC_B   ,KC_J   ,KC_J   ,KC_L   ,KC_SLSH,KC_RSFT,
+        KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_F   ,                             KC_B   ,KC_J   ,KC_J   ,KC_L   ,KC_SLSH,KC_LALT,
     // |-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------+-------|
                                         RAISE  ,KC_SPC ,TENKEY,      SECOND ,KC_ENT ,LOWER
     //                                 `-------+-------+-------|    |-------+-------+-------'
